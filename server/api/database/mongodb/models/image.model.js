7bd5a74e7085245dabd3f6bd939454fa7c4c4b7a
= <=>
2021-03-16 14:59:36 +0700

    fix for heroku

--- server/api/database/mongodb/models/image.model.js
+++ server/api/database/mongodb/models/image.model.js
@@ -1,4 +1,4 @@
-const ImageSchema = require('../schemas/image.schema');
+const ImageSchema = require('..\\schemas\\image.schema');
 
 module.exports = {
   schema: ImageSchema, apply: mongoose => mongoose.model('image', ImageSchema)
