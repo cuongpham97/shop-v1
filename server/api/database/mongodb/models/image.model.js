e5049221096c7a9efaba07e268435f1267b79f2e
= <=>
2021-03-16 15:01:41 +0700

    fix package.json for heroku

--- server/api/database/mongodb/models/image.model.js
+++ server/api/database/mongodb/models/image.model.js
@@ -1,4 +1,4 @@
-const ImageSchema = require('..\\schemas\\image.schema');
+const ImageSchema = require('../schemas/image.schema.js');
 
 module.exports = {
   schema: ImageSchema, apply: mongoose => mongoose.model('image', ImageSchema)
